6052ce6ce07b96967bde5f1eee6c4231623c13c6
valentinconan <valentin.conan@me.com>
2024-07-05 10:59:54 +0200

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const messageQueue = require('./bull/queue-config');
+const consumer = require("./bull/consumer")
+
+const app = express();
+const port = 3000;
+
+// Middleware pour parser le JSON
+app.use(express.json());
+
+//start consumer
+consumer();
+
+// Route order to produce message for test purpose
+app.get('/produce', async (req, res) => {
+    const message = {"date": new Date().getTime()};
+
+    // Add message to queue
+    await messageQueue.add(message);
+
+    console.log('Message produced:', message);
+    res.status(200).send({ status: 'Message added to queue', message });
+});
+
+// Route order to produce error message for test purpose
+app.get('/error', async (req, res) => {
+    const message = {error:true};
+
+    // Add message to queue
+    await messageQueue.add(message);
+
+    console.log('Message produced:', message);
+    res.status(200).send({ status: 'Message added to queue', message });
+});
+
+// Démarre le serveur
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
